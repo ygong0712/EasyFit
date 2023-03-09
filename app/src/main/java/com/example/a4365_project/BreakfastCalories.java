b6ce77879193fe9960c745631af16679f895b1b4
Feilian Huang <78404725+EDGAhab@users.noreply.github.com>
2023-03-09 15:04:26 -0500

    Calorie Calculator

--- app/src/main/java/com/example/a4365_project/BreakfastCalories.java
+++ app/src/main/java/com/example/a4365_project/BreakfastCalories.java
@@ -1,7 +1,6 @@
 package com.example.a4365_project;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,36 +13,38 @@ import com.google.firebase.auth.FirebaseAuth;
 public class BreakfastCalories extends AppCompatActivity {
 
     Button button;
-    FirebaseAuth mAuth;
     EditText foodt, foodc;
-
-    public static final String CAL_PREFERENCES = "CalPrefs";
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.breakfast_calories);
 
+        String calType = "breakfast";
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            calType = extras.getString("type");
+        }
+
         button = findViewById(R.id.confirmbutton);
 
         foodt = findViewById(R.id.foodtype);
         foodc = findViewById(R.id.food1calories);
-        String breakfastNameAdd= foodt.getText().toString();
-        String breakfastCalAdd = foodc.getText().toString();
 
-        SharedPreferences settings = getApplicationContext().getSharedPreferences(CAL_PREFERENCES, MODE_PRIVATE);
-        SharedPreferences.Editor editor = settings.edit();
-
+        String finalCalType = calType;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //do something
-                editor.putString("breakfastName", breakfastNameAdd);
-                editor.putString("breakfastCal", breakfastCalAdd);
+                String breakfastNameAdd= foodt.getText().toString();
+                String breakfastCalAdd = foodc.getText().toString();
+
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("cal", breakfastCalAdd);
+                intent.putExtra("type", finalCalType);
                 startActivity(intent);
                 finish();
             }
         });
+
     }
 }
--- app/src/main/java/com/example/a4365_project/MainActivity.java
+++ app/src/main/java/com/example/a4365_project/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.a4365_project;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
+    public static final String CAL_PREFERENCES = "CalPrefs";
     Button buttonl;  //logout button
     Button buttonc;  //clear button
     FloatingActionButton button1; //breakfast
@@ -22,12 +24,20 @@ public class MainActivity extends AppCompatActivity {
     FloatingActionButton button3; //training
     FloatingActionButton button4; //dinner
     FloatingActionButton button5; //snack
-    TextView text, item1, item2, item3, item4, item5;
+    TextView text, item1, item2, item3, item4, item5, exerciseSum, foodSummation, totalSummation;
     FirebaseUser user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences settings = getApplicationContext().getSharedPreferences(CAL_PREFERENCES, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        //editor.putString("breakfast", "0");
+        //editor.putString("lunch", "0");
+        //editor.putString("exercise", "0");
+        //editor.putString("dinner", "0");
+        //editor.putString("snack", "0");
 
         mAuth = FirebaseAuth.getInstance();
         buttonl = findViewById(R.id.logout);
@@ -44,6 +54,62 @@ public class MainActivity extends AppCompatActivity {
         item3 = findViewById(R.id.item3);
         item4 = findViewById(R.id.item4);
         item5 = findViewById(R.id.item5);
+
+        exerciseSum = findViewById(R.id.activitySum);
+        foodSummation = findViewById(R.id.foodTotal);
+        totalSummation = findViewById(R.id.Summation);
+
+
+        //update the calories
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            String calValue = extras.getString("cal");
+            String caltype = extras.getString("type");
+            if(caltype.equals("breakfast")) {
+                //item1.setText("Breakfast: " + calValue);
+                editor.putString("breakfast", calValue);
+            } else if (caltype.equals("lunch")){
+                //item2.setText("Lunch: " + calValue);
+                editor.putString("lunch", calValue);
+            } else if (caltype.equals("exercise")){
+                //item3.setText("Exercise: " + calValue);
+                editor.putString("exercise", calValue);
+            } else if (caltype.equals("dinner")){
+                //item4.setText("Dinner: " + calValue);
+                editor.putString("dinner", calValue);
+            } else if (caltype.equals("snack")){
+                //item5.setText("Snack: " + calValue);
+                editor.putString("snack", calValue);
+            }
+            editor.commit();
+        } else {
+            editor.putString("breakfast", "0");
+            editor.putString("lunch", "0");
+            editor.putString("exercise", "0");
+            editor.putString("dinner", "0");
+            editor.putString("snack", "0");
+            editor.commit();
+        }
+
+        //update UI
+        String breakfastCalories = settings.getString("breakfast", null);
+        String lunchCalories = settings.getString("lunch", null);
+        String exerciseCalories = settings.getString("exercise", null);
+        String dinnerCalories = settings.getString("dinner", null);
+        String snackCalories = settings.getString("snack", null);
+        int foodSum = Integer.parseInt(breakfastCalories) + Integer.parseInt(lunchCalories)
+                + Integer.parseInt(dinnerCalories) + Integer.parseInt(snackCalories);
+        int totalSum = foodSum - Integer.parseInt(exerciseCalories);
+        item1.setText("Breakfast: " + breakfastCalories);
+        item2.setText("Lunch: " + lunchCalories);
+        item3.setText("Exercise: " + exerciseCalories);
+        item4.setText("Dinner: " + dinnerCalories);
+        item5.setText("Snack: " + snackCalories);
+        exerciseSum.setText(exerciseCalories);
+        foodSummation.setText(Integer.toString(foodSum));
+        totalSummation.setText(Integer.toString(totalSum));
+
+
 
 
         text = findViewById(R.id.user);
@@ -78,6 +144,44 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), BreakfastCalories.class);
+                intent.putExtra("type", "breakfast");
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BreakfastCalories.class);
+                intent.putExtra("type", "lunch");
+                startActivity(intent);
+                finish();
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BreakfastCalories.class);
+                intent.putExtra("type", "exercise");
+                startActivity(intent);
+                finish();
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BreakfastCalories.class);
+                intent.putExtra("type", "dinner");
+                startActivity(intent);
+                finish();
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), BreakfastCalories.class);
+                intent.putExtra("type", "snack");
                 startActivity(intent);
                 finish();
             }
