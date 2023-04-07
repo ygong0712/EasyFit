b26e975da2516b3ed70eb8e1816934b8d88a4da5
ygong0712 <derrickgong0712@gmail.com>
2023-04-07 15:26:08 -0400

    food plan

--- app/src/main/java/com/example/a4365_project/Foodplan.java
+++ app/src/main/java/com/example/a4365_project/Foodplan.java
@@ -1,6 +1,10 @@
 package com.example.a4365_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +17,26 @@ public class Foodplan extends AppCompatActivity {
     private  Integer weight;
     private  Integer height;
     private  Integer goal;
-
+    private TextView bc, bp, bf, lc, lp, lf, dc, dp, df, bm, lm, dm;
+    private Button share;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.foodplan);
+        bc = findViewById(R.id.bcarb);
+        bp = findViewById(R.id.bprotein);
+        bf = findViewById(R.id.bfat);
+        lc = findViewById(R.id.lcarb);
+        lp = findViewById(R.id.lprotein);
+        lf = findViewById(R.id.lfat);
+        dc = findViewById(R.id.dcarb);
+        df = findViewById(R.id.dfat);
+        dp = findViewById(R.id.dprotein);
+        bm = findViewById(R.id.bm);
+        lm = findViewById(R.id.lm);
+        dm = findViewById(R.id.dm);
+        share = findViewById(R.id.share);
+
 
         mAuth = FirebaseAuth.getInstance();
         Bundle extras = getIntent().getExtras();
@@ -27,5 +46,56 @@ public class Foodplan extends AppCompatActivity {
             height = extras.getInt("height");
             goal = extras.getInt("goal");
         }
+
+        Integer goalcarb = (int) (1 * weight);
+        Integer bcarb = (int) (0.5 * goalcarb);
+
+        Integer goalfat = (int) (0.5 * weight);
+        Integer bfat = (int) (0.2*weight);
+        Integer bbfat = (int) (0.1*weight);
+
+        Integer goalprotein = (int) (1.5 * weight);
+        Integer bprotein = (int) (0.5 * weight);
+
+
+
+        bc.setText(bcarb.toString() + "g");
+        lc.setText(bcarb.toString() +"g");
+        dc.setText("0" + "g");
+
+        bp.setText(bprotein.toString() + "g");
+        lp.setText(bprotein.toString() + "g");
+        dp.setText(bprotein.toString() + "g");
+
+        bf.setText(bfat.toString() + "g");
+        lf.setText(bfat.toString() + "g");
+        df.setText(bbfat.toString() + "g");
+
+
+        Integer oat = (bcarb / 12) * 100;
+        Integer egg = (bprotein /13) * 100;
+        bm.setText("Example menu: " + "oatmeal " + oat.toString() +"g" + ", " + "egg " + egg.toString() +"g");
+
+        Integer chicken = (bprotein / 31) * 100;
+        Integer rice = (bcarb / 28) * 100;
+        lm.setText("Example menu: " + "rice " + rice.toString() +"g" +", " + "chicken " + chicken.toString() +"g" + ", " + "veggies");
+
+        Integer beef = (bprotein / 22) * 100;
+        dm.setText("Example menu: " + "beef " + beef.toString() + "g" + ", " + "veggies");
+
+
+        // share the food plan with other users using firebase
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Foodplan.class);
+                intent.putExtra("age", age);
+                intent.putExtra("weight", weight);
+                intent.putExtra("height", height);
+                intent.putExtra("goal", goal);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
